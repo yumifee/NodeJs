b7e1e6f7c484595edf74456b66ecb1bdbaa3d8f8
yumifee <yumifebriana3@gmail.com>
2022-08-22 14:35:45 +0700

    first commit

---
+++ testnode.js
@@ -0,0 +1,10 @@
+console.log('Hai aku Surty Cantiex <3');
+
+const fs = require('fs') //require is a global variable provided to all your Node.js scripts, so you can use it anywhere you like
+fs.writeFileSync('notes.txt', 'Hai aku Surty Cantiex <3')
+
+// const check = function () {
+//     console.log('Doing some work...')
+//    }
+//    module.exports = check
+
